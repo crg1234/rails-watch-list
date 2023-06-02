93f517cc6f7626f801ca71876549b7736ea9e6e9
Christopher Grant <christopher.ryan.grant@gmail.com>
2023-06-02 11:52:31 +0200

    updating root

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,5 @@ Rails.application.routes.draw do
   # get 'lists/new', to: 'lists#new', as: :new_list
   # get 'lists/:id', to: 'lists#show', as: :list
   # post 'lists', to: 'lists#create'
+  root to: 'lists#index'
 end
